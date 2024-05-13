860941326b67168622789b3879e5ba168b08bea6
jasmine17 <jmmerchant4@gmail.com>
2024-05-13 19:11:28 +0800

    Add files via upload

---
+++ UnitTestCourse/FirstTestClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestCourse
+{
+    public class FirstTestClass
+    {
+        public string HelloWorld()
+        {
+            // Function Logic
+
+            return "Hello World";
+        }
+
+        public decimal AddTwoNumbers(decimal a, decimal b)
+        {
+            decimal c = a - b;
+            return c;
+        }
+    }
+}
